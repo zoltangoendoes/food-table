0c3663595fd8a5f0ac75f4491303ed7133f67146
zoltangoendoes <61847952+zoltangoendoes@users.noreply.github.com>
2022-09-30 12:28:11 +0200

    add accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatIconModule],
+  imports: [BrowserModule, FormsModule, MatIconModule, MatExpansionModule, BrowserAnimationsModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
